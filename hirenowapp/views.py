2dd31f0f8b89d9ca98741d6e2b9f89900bd324a4
Khaled Md Tuhidul Hossain <me@tuhidulhossain.com>
2024-07-14 01:40:22 +0800

    Update views.py

--- hirenowapp/views.py
+++ hirenowapp/views.py
@@ -5,6 +5,11 @@ from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
+
+
+def index(request):
+    return render(request,'index.html')
+
 @api_view(['GET'])
 def hello_world(request):
     return Response({'message': 'Hello, world!'})
