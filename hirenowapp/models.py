a31708841ac8ee109546eef4b26e27c8aa1f1108
Khaled <me@tuhidulhossain.com>
2024-07-19 23:51:02 +0800

    Update models.py

--- hirenowapp/models.py
+++ hirenowapp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+
+# api/models.py
+
+from django.db import models
+
+class CV(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    cv = models.FileField(upload_to='cvs/')
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
